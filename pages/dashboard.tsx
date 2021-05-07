b12906c957834db07ca308c18a31e80a8510578c
Felipe Ribeiro <piporibeiro@gmail.com>
2021-05-07 00:01:58 -0300

    feat: refresh token and error treat error

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,8 +1,16 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { AuthContext } from "../contexts/AuthContext";
+import { api } from "../services/api";
 
 export default function Dashboard() {
   const { user } = useContext(AuthContext);
+
+  useEffect(() => {
+    api.get("/me").then((response) => {
+      console.log(response);
+    });
+  }, []);
+
   return (
     <div>
       <h1>Dashboard</h1>
--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,7 @@
-import axios from "axios";
-import { parseCookies } from "nookies";
+import axios, { AxiosError } from "axios";
+import { parseCookies, setCookie } from "nookies";
 
-const cookies = parseCookies();
+let cookies = parseCookies();
 
 export const api = axios.create({
   baseURL: "http://localhost:3333",
@@ -9,3 +9,52 @@ export const api = axios.create({
     Authorization: `Bearer ${cookies["nextauth.token"]}`,
   },
 });
+
+// o response.use recebe dois parâmetros
+// O primeiro é se a reposta der sucesso que no caso vou manter como esta
+// O segundo é o que que eu quero fazer se a resposta der erro
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error: AxiosError) => {
+    if (error.response.status === 401) {
+      if (error.response.data?.code === "token.expired") {
+        // renovar o token
+        cookies = parseCookies();
+
+        // vamos criar uma fila de requisições para que a aplicação pause até
+        // que todas as requisições sejam feitas para depois renovar o token
+
+        const { "nextauth.refreshToken": refreshToken } = cookies;
+        api
+          .post("/refresh", {
+            refreshToken,
+          })
+          .then((response) => {
+            const { token } = response.data;
+
+            setCookie(undefined, "nextauth.token", token, {
+              maxAge: 60 * 60 * 24 * 30, // 30 dias
+              path: "/",
+            });
+
+            setCookie(
+              undefined,
+              "nextauth.refreshToken",
+              response.data.refreshToken,
+              {
+                maxAge: 60 * 60 * 24 * 30, // 30 dias
+                path: "/",
+              }
+            );
+
+            api.defaults.headers["Authorization"] = `Bearer ${token}`;
+          });
+      } else {
+        // deslogar o usuário
+      }
+    }
+  }
+);
