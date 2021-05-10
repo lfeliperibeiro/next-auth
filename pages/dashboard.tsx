f3c0b79d564bb2e3fb8258feeb0d8de3a4e572e5
Felipe Ribeiro <piporibeiro@gmail.com>
2021-05-10 20:43:19 -0300

    refactor: remove console.log

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -28,8 +28,6 @@ export default function Dashboard() {
 export const getServerSideProps = withSSRAuth(async (ctx) => {
   const apiClient = setupAPIClient(ctx);
   const response = await apiClient.get("/me");
-
-  console.log(response.data);
   return {
     props: {},
   };
