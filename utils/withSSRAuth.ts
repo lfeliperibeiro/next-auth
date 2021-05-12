b133b54086175e6286a30dc92c59f1255b5cfc96
Felipe Ribeiro <piporibeiro@gmail.com>
2021-05-11 21:21:49 -0300

    refactor: move conditional for separate file

--- utils/withSSRAuth.ts
+++ utils/withSSRAuth.ts
@@ -5,13 +5,24 @@ import {
 } from "next";
 import { destroyCookie, parseCookies } from "nookies";
 import { AuthTokenError } from "../services/errors/AuthTokenError";
+import decode from "jwt-decode";
+import { validateUserPermissions } from "./validadeUserPermissions";
 
-export function withSSRAuth<P>(fn: GetServerSideProps<P>) {
+type WithSSRAuthOptions = {
+  permissions?: string[];
+  roles?: string[];
+};
+
+export function withSSRAuth<P>(
+  fn: GetServerSideProps<P>,
+  options?: WithSSRAuthOptions
+) {
   return async (
     ctx: GetServerSidePropsContext
   ): Promise<GetServerSidePropsResult<P>> => {
     const cookies = parseCookies(ctx);
-    if (!cookies["nextauth.token"]) {
+    const token = cookies["nextauth.token"];
+    if (!token) {
       return {
         redirect: {
           destination: "/",
@@ -19,6 +30,27 @@ export function withSSRAuth<P>(fn: GetServerSideProps<P>) {
         },
       };
     }
+
+    if (options) {
+      const user = decode<{ permissions: string[]; roles: string[] }>(token);
+      const { permissions, roles } = options;
+
+      const userHasValidPermissions = validateUserPermissions({
+        user,
+        permissions,
+        roles,
+      });
+
+      if (!userHasValidPermissions) {
+        return {
+          redirect: {
+            destination: "/dashboard",
+            permanent: false,
+          },
+        };
+      }
+    }
+
     try {
       return await fn(ctx);
     } catch (err) {
